0545422feb61206acb05317f5f48ac5d62640fac
Allegra Pusateri <allegrapusateri@gmail.com>
2019-02-24 21:58:26 -0600

    I can delete things now!

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 $(function(){
     //set up event listener
     $('#js-shopping-list-form').submit(event => {
-        
         //stop default form submissions
         event.preventDefault();
-        
         //use .val to grab current value of form input
-        const newItem = $('#js-shopping-list-entry').val();
+        const newItem = $('#shopping-list-entry').val();
         
         //clear text from form input so you can do another one
-        $('#js-shopping-list-entry').val('');
+        $('#shopping-list-entry').val('');
 
         //add new list item as a new <li>
         $('.shopping-list').append(
-            '<li>
-                <span class="shopping-item">${newItem}</span>
-                <div class="shopping-item-controls">
-                    <button class="shopping-item-toggle">
-                        <span class="button-label">check</span>
-                    </button>
-                    <button class="shopping-item-delete">
-                        <span class="button-label">delete</span>
-                    </button>
-                </div>
-            </li>'
+            `<li>
+            <span class="shopping-item">${newItem}</span>
+            <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                </button>
+                <button class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                </button>
+            </div>
+        </li>`
         );
+    });
+
+    //listen for click event, delete button edition
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+        
+        //listening to parent element, so this will find the closest <li> and remove it
+        $(this).closest('li').remove();
+    });
+
+    //listen for click event, check button edition
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+        
+        //
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item-checked');
     });
 })
