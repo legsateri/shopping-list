433ef3e395f391fd651e1b93fb6b9f0c4d84885e
Allegra Pusateri <allegrapusateri@gmail.com>
2019-02-24 22:09:01 -0600

    Fixed a small bug and can now check things off

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/*INSTRUCTIONS*/
+
+//modify this shopping list app so users can do the following:
+    //enter items they need to purchase by entering text and hittin "return" or clicking the "add item" button
+    //check and uncheck items on the list by clicking the "check" button
+    //permanently remove items from the list
+
 $(function(){
     //set up event listener
     $('#js-shopping-list-form').submit(event => {
@@ -5,10 +12,8 @@ $(function(){
         event.preventDefault();
         //use .val to grab current value of form input
         const newItem = $('#shopping-list-entry').val();
-        
         //clear text from form input so you can do another one
         $('#shopping-list-entry').val('');
-
         //add new list item as a new <li>
         $('.shopping-list').append(
             `<li>
@@ -27,15 +32,14 @@ $(function(){
 
     //listen for click event, delete button edition
     $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
-        
         //listening to parent element, so this will find the closest <li> and remove it
         $(this).closest('li').remove();
     });
 
     //listen for click event, check button edition
     $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
-        
-        //
-        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item-checked');
+        //finds the closest <li> and toggles it so it's crossed out. 
+        //question: i did this exact thing but as ".shopping-item__checked" initially because it's a class in the html but it didn't work until after I remoed the ".", not sure why. is it something to do with it being the second class for the element?
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
     });
 })
